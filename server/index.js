59c5b892c02219e18efc1b4bf0885b75d34100bc
alias722 <wei@sivir.pw>
2021-04-19 23:59:01 +0800

    google dialog with express

--- server/index.js
+++ server/index.js
@@ -6,17 +6,29 @@ const {
 	Table,
 	Carousel
 } = require('actions-on-google')
+const bodyParser = require('body-parser')
+
+express().use(bodyParser.json())
 
 const port = 21434
-
 const app = dialogflow({
 	debug: true
 })
-
+//If recieved GET request to '/'
+//return good
+//Any request to /webhook
+//will be retreived to dialog
 express().get('/',(req,res)=>{
 	res.sendStatus(200);
 })
+express().post('/webhook',app)
 
 express().app.listen(port,()=>{
 	console.log("Server running on port "+port)
+})
+
+
+
+app.intent('intent_name',(conv)=>{
+	conv.ask('How are you?')
 })
