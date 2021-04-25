7f621b0d581b92c823556dab1625d5299170fd51
alias722 <wei@sivir.pw>
2021-04-25 11:38:32 +0800

    bug fix

--- server/index.js
+++ server/index.js
@@ -7,30 +7,33 @@ const {
 	Carousel
 } = require('actions-on-google')
 const bodyParser = require('body-parser')
-
-express().use(bodyParser.json())
-
 const port = 21434
+const expressapp = express()
 const app = dialogflow({
 	debug: true
 })
+
+
 //If recieved GET request to '/'
 //return good
 //Any request to /webhook
 //will be retreived to dialog
-express().get('/',(req,res)=>{
+expressapp.get('/',(req,res)=>{
 	res.sendStatus(200);
 })
-express().post('/webhook',app)
-express().get('*',(req,res)=>{
-	res.redirect(302,'/')
-})
-express().listen(port,()=>{
-	console.log("Server running on port "+port)
-})
-
-
+//express().use(bodyParser.json())
+expressapp.post('/webhook',bodyParser,app)
 
 app.intent('intent_name',(conv)=>{
 	conv.ask('How are you?')
+})
+
+expressapp('*',(req,res)=>{
+	res.redirect(302,'/')
+})
+
+
+
+expressapp.listen(port,()=>{
+	console.log("Server running on port "+port)
 })
